43fecb48fc9e53360f0f4a43dc97cc867121e3c6
flyahn06 <49788256+flyahn06@users.noreply.github.com>
2021-03-06 19:29:27 +0900

    기본적인 토스트 알림 띄움

---
+++ main.py
@@ -0,0 +1,26 @@
+import weather_getter
+import win10toast
+from time import sleep
+import pprint
+
+code, current_weather = weather_getter.generateRequest()
+
+pprint.pprint(current_weather)
+
+def clicked():
+    print("Clicked!")
+
+
+toaster = win10toast.ToastNotifier()
+print()
+toaster.show_toast(
+    f'{current_weather["weather"][0]["description"]}, {current_weather["main"]["temp"]}°C',
+    f'클릭하여 더 자세한 정보를 만나보세요',
+    icon_path=f'sources\\image\\{current_weather["weather"][0]["icon"]}.ico',
+    duration=3,
+    threaded=True,
+    callback_on_click=clicked
+)
+
+while toaster.notification_active():
+    sleep(0.1)
