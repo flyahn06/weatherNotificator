ddbfae43c9a5fa65c98f92da9f189266faf0f7c4
flyahn06 <49788256+flyahn06@users.noreply.github.com>
2021-03-06 19:30:31 +0900

    API 요청 완성

--- weather_getter.py
+++ weather_getter.py
@@ -1,3 +1,35 @@
-import json
+import requests
 import base64
+import json
 
+APIKEY = base64.b64decode(open("sources/apikey", 'r').read().encode()).decode()
+url = "http://api.openweathermap.org/data/2.5/weather"
+query = {
+    "appid": APIKEY,
+    'q': "Anyang-si,KR",
+    'units': "metric",
+    'lang': "KR"
+}
+
+
+class APIRequestError(Exception):
+    __module__ = Exception.__module__
+
+
+def generateRequest(**data):
+    res = requests.request("GET", url, params=query, data=json.dumps(data))
+    respond = json.loads(res.text)
+
+    if str(res.status_code).startswith("4"):
+        raise APIRequestError(f"{res.status_code} {respond['message']}")
+
+    return res.status_code, respond
+
+
+def getAPIKey():
+    return APIKEY
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(generateRequest())
